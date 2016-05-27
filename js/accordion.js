95b34c028d060d489e013afd2731064584b3d1fd
Alexander Sadleir <maxious@lambdacomplex.org>
2016-05-27 13:37:05 +1000

    Add responsive accordian submenu and associated grid changes

--- js/accordion.js
+++ js/accordion.js
@@ -3,3 +3,8 @@ $('.js-accordion-trigger').bind('click', function(e){
   jQuery(this).parent().toggleClass('is-expanded');
   e.preventDefault();
 });
+$('.accordion-heading').bind('touchstart click', function(e){
+    jQuery(this).parent().parent().find('.secondary-menu').slideToggle('fast');  // apply the toggle to the ul
+    jQuery(this).parent().toggleClass('is-expanded');
+    e.preventDefault();
+});
